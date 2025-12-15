4693531aa46e24599063bb8a6c9dab39ad26b1f0
Mantagute <j285570@dac.unicamp.br>
2025-12-15 10:29:23 -0300

    we're going to

---
+++ main.js
@@ -0,0 +1,25 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emialInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(nameInput.value === ''|| emialInput.value === '') {
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000)
+    }
+    else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emialInput.value}`));
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emialInput.value = '';
+    }
+}
