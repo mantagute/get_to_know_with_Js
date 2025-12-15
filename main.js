1b87889c490f5e4b34bc757ce8ef52f15fd3655d
Mantagute <j285570@dac.unicamp.br>
2025-12-15 15:44:18 -0300

    add, modify, and remove

--- main.js
+++ main.js
@@ -4,10 +4,37 @@
 
 // title.style.color = 'red'
 
-const listItems = document.querySelectorAll('.list-items');
+// const listItems = document.querySelectorAll('.list-items');
 
-for (i = 0; i < listItems.length ; i++){
-    listItems[i].style.color = 'red'
-}
+// for (i = 0; i < listItems.length ; i++){
+//     listItems[i].style.color = 'red'
+// }
 
-console.log(listItems);
+// console.log(listItems);
+
+// Creating Element
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+// Adding Element
+
+ul.append(li);
+
+// Modifying the text
+
+li.innerText = 'X-men';
+
+// Modifying Attributes & Classes  
+
+// li.setAttribute('id', 'main-heading');
+// li.removeAttribute('id');
+
+// const title = document.querySelector('#main-heading');
+
+// console.log(title.getAttribute('id'))
+
+li.classList.add('list-items')
+
+console.log(li.classList.contains('list-items'))
+
+li.remove()
