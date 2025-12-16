52b08ef55c315e32549ce1b6edae693db3e64dd6
Mantagute <j285570@dac.unicamp.br>
2025-12-16 14:45:36 -0300

    Event Propagation

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-// DOM MANIPULATION
-const revealBtn = document.querySelector('.reveal-btn');
-const hiddenContent = document.querySelector('.hidden-content');
+//DOM MANIPULATION
 
-function revealContent() {
-    if (hiddenContent.classList.contains('show')) {
-        hiddenContent.classList.remove('show'); 
-        revealBtn.classList.remove('active');   
-    } else {
-        hiddenContent.classList.add('show');   
-        revealBtn.classList.add('active'); 
-    }
-}
+//EVENT PROPAGATION
 
-revealBtn.addEventListener('click', revealContent);
+window.addEventListener('click', function() {
+    console.log('Window');
+}, false);
 
+document.addEventListener('click', function() {
+    console.log('Document')
+}, false);
+
+document.querySelector('.div2').addEventListener('click', function(){
+    // e.stopPropagation();
+    console.log('DIV 2');
+}, false);
+
+document.querySelector('.div1').addEventListener('click', function(){
+    console.log('DIV 1')
+}, {once: true});
+
+document.querySelector('button').addEventListener('click', function(e){
+    console.log(e.target.innerText = 'clicked');
+}, false);
 
 
