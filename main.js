fdd61f362ffbe13b07bacc6f1abad0673120ce98
Mantagute <j285570@dac.unicamp.br>
2025-12-16 20:02:36 -0300

    timer html and css

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 // VARIABLES
-
-const accordion = document.getElementsByClassName('content-container');
-
-for (let i = 0 ; i < accordion.length ; i++){
-    accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active');
-    })
-}
