3ce229e295eb1be513a0c27a9330c1a954d391fb
Mantagute <j285570@dac.unicamp.br>
2025-12-18 14:20:09 -0300

    css variables updated whit js mini-project

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
-const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const inputs = document.querySelectorAll('.controls input');
 
-function setDate() {
-    const now = new Date();
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds/60) * 360) + 90;
-
-    if(secondsDegrees === 90) {
-        secondHand.style.transition = 'none';
-    } else {
-        secondHand.style.transition = 'all 0.05s';
-    }
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-
-
-    const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes/60) * 360) + 90;
-    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
-    const hours = now.getHours();
-    const hoursDegrees = ((hours/12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
-setInterval(setDate, 1000,);
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
