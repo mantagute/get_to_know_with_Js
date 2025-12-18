baac80870ace8e688971d88acefdcc97b4768c8a
Mantagute <j285570@dac.unicamp.br>
2025-12-18 16:32:18 -0300

    inital html, css and js

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-const panels = document.querySelectorAll('.panel');
-
-function toggleOpen() {
-    this.classList.toggle('open');
-}
-
-function toggleActive(e) {
-    if (e.propertyName.includes('flex')){
-        this.classList.toggle('open-active')
-    }
-}
-
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
-
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
