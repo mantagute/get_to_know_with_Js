f291881d88a1905ed4826f67bde8d3a0f503c690
Mantagute <j285570@dac.unicamp.br>
2025-12-19 13:02:48 -0300

    inital files for playing with arrays 2

--- main.js
+++ main.js
@@ -1,40 +1,28 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  // ## Array Cardio Day 2
 
-const cities = [];
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
 
-fetch(endpoint)
-    .then(blob => blob.json()) 
-    .then(data => cities.push(...data));
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
 
-function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex);
-    });
-}
+  // Some and Every Checks
+  // Array.prototype.some() // is at least one person 19 or older?
+  // Array.prototype.every() // is everyone 19 or older?
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+  // Array.prototype.find()
+  // Find is like filter, but instead returns just the one you are looking for
+  // find the comment with the ID of 823423
 
-function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-        return `
-            <li>
-                <span class="name">${cityName}, ${stateName}</span> 
-                <span class="population">${numberWithCommas(place.population)}</span> 
-            </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
-}
-
-const search = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-search.addEventListener('change', displayMatches);
-search.addEventListener('keyup', displayMatches);
+  // Array.prototype.findIndex()
+  // Find the comment with this ID
+  // delete the comment with the ID of 823423
