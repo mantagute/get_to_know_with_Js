9cb3db02a7c815c16728420030dbc28f611340f6
Mantagute <j285570@dac.unicamp.br>
2025-12-19 13:10:17 -0300

    .some method

--- main.js
+++ main.js
@@ -17,6 +17,20 @@ const comments = [
 
   // Some and Every Checks
   // Array.prototype.some() // is at least one person 19 or older?
+    // const isAdult = people.some(function(person){
+    //     const currentYear = new Date().getFullYear();
+    //     if( currentYear - person.year >= 19){
+    //         return true;
+    //     }
+    // })
+
+    // another simple way  
+
+    const isAdult = people.some(person => {
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.year >= 19;
+    });
+    
   // Array.prototype.every() // is everyone 19 or older?
 
   // Array.prototype.find()
