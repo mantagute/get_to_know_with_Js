e1e7757c0ab642004c03c27e8e3170b044d9a384
Mantagute <j285570@dac.unicamp.br>
2025-12-26 17:43:51 -0300

    initial html, css and js for webcam mini-project

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-    const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-
-    function strip(bandName){
-        return bandName.replace(/^(a |the |an )/i, '').trim();
-    }
-
-    const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
-
-    document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('')
-
-    console.log(sortedBands);   
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
