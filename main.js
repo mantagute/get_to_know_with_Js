febc835091d2dc3b0a0e953cee5e871b21e06f90
Mantagute <j285570@dac.unicamp.br>
2025-12-26 19:05:55 -0300

    voice detection mini-project

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join('');
+
+    p.textContent = transcript;
+
+    if(e.results[0].isFinal){
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    
+    if(transcript.includes('.unicorn')){
+        console.log('🦄🦄🦄🦄🦄🦄🦄🦄')
+    }
+
+    console.log(transcript);
+})
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
